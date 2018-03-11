c57b4023d9beeebd586a96cccedafadb45ec487d
zub4t <30879430+zub4t@users.noreply.github.com>
2018-03-11 08:30:39 -0400

    Add files via upload

---
+++ map.php
@@ -0,0 +1,19 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>isolocaliza</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div id="map">
+            <img src="" class="image">
+            <img src="" class="image">
+            <img src="" class="image">
+        </div>
+        
+    </body>
+     <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCEQsqujVt5jhubQjf545HO9e2hNtR8API&callback=initMap">
+    </script>
+    
+</html>
